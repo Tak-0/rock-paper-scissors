2e00d02fc3fa9c8fc98c4d611e60257482dfa717
Tak <185003586+Tak-0@users.noreply.github.com>
2025-04-12 16:37:10 +0100

    finished the get computer choice function and also completed the get human choice function

--- script.js
+++ script.js
@@ -1,18 +1,46 @@
-console.log()
 
-function getComputerChoice(choice) {
-    if (Math.random() <=0.333) {
-        let choice = "rock"
+
+function getComputerChoice() {
+    let a = Math.random()
+    
+    if (a <=0.333) {
+        choice = "rock"
     }
 
-    else if (Math.random() > 0.333 && Math.random() <= 0.666) {
-        let choice = "paper"
+    else if (a > 0.333 && a <= 0.666) {
+        choice = "paper"
     }
 
     else {
-        let choice = "scissors"
+        choice = "scissors"
     }
+    
+    console.log(`The computer chooses ${choice}`)
+
     return choice
 }
 
 
+
+function getHumanChoice() {
+    const input = prompt(`What do you choose? Rock, Paper or Scissors?`)
+    let choice
+    if (input === `Rock` ||input ===`rock` ) {
+        choice = `rock`
+    }
+    else if (input === `Paper` ||input === `paper` ) {
+        choice = `paper`
+    }
+    else if (input === `Scissors` ||input === `scissors` ) {
+        choice = `scissors`
+    }
+    else {
+        console.log(`Please choose a valid option`)
+    }
+
+    console.log(`You chose ${choice}`)
+    return choice
+
+}
+
+getHumanChoice()
