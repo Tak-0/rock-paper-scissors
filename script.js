6c9506a35055ab957a678a457f10b4a30da3296b
Tak <185003586+Tak-0@users.noreply.github.com>
2025-04-16 00:56:33 +0100

    created the function to play a round

--- script.js
+++ script.js
@@ -1,46 +1,103 @@
 
 
 function getComputerChoice() {
-    let a = Math.random()
-    
+    let a = Math.random()  
+    let cChoice
     if (a <=0.333) {
-        choice = "rock"
+        cChoice = "rock"
     }
 
     else if (a > 0.333 && a <= 0.666) {
-        choice = "paper"
+        cChoice = "paper"
     }
 
     else {
-        choice = "scissors"
+        cChoice = "scissors"
     }
     
-    console.log(`The computer chooses ${choice}`)
+    console.log(`The computer chooses ${cChoice}`)
 
-    return choice
+    return cChoice
 }
 
 
-
 function getHumanChoice() {
-    const input = prompt(`What do you choose? Rock, Paper or Scissors?`)
-    let choice
-    if (input === `Rock` ||input ===`rock` ) {
-        choice = `rock`
+    const input = prompt(`What do you choose? Rock, Paper or Scissors?`).toLowerCase()
+    let hChoice
+    if (input ===`rock` ) {
+        hChoice = `rock`
     }
-    else if (input === `Paper` ||input === `paper` ) {
-        choice = `paper`
+    else if (input === `paper` ) {
+        hChoice = `paper`
     }
-    else if (input === `Scissors` ||input === `scissors` ) {
-        choice = `scissors`
+    else if (input === `scissors` ) {
+        hChoice = `scissors`
     }
     else {
         console.log(`Please choose a valid option`)
     }
 
-    console.log(`You chose ${choice}`)
-    return choice
+    console.log(`You chose ${hChoice}`)
+    return hChoice
 
 }
 
-getHumanChoice()
+
+
+
+function playRound(cChoice, hChoice) {
+    
+    if (hChoice == "rock" && cChoice == "paper") {
+        computerScore+=1
+        console.log(`Human:${humanScore} Computer:${computerScore}`)
+    }
+
+    else if (hChoice == "rock" && cChoice == "scissors") {
+        humanScore+=1
+        console.log(`Human:${humanScore} Computer:${computerScore}`)
+    }
+
+    else if (hChoice == "paper" && cChoice == "scissors") {
+        computerScore+=1
+        console.log(`Human:${humanScore} Computer:${computerScore}`)
+    }
+
+    else if (hChoice == "paper" && cChoice == "rock") {
+        humanScore+=1
+        console.log(`Human:${humanScore} Computer:${computerScore}`)
+    }
+
+    else if (hChoice == "scissors" && cChoice == "rock") {
+        computerScore+=1
+        console.log(`Human:${humanScore} Computer:${computerScore}`)
+    }
+
+    else if (hChoice == "scissors" && cChoice == "paper") {
+        humanScore+=1
+        console.log(`Human:${humanScore} Computer:${computerScore}`)
+    }
+
+    else {
+        //console.log(`Try again`)
+        console.log(`Human:${humanScore}Computer:${computerScore}`) 
+        
+    }
+    
+
+}
+
+function playGame() {
+    humanScore = 0
+    computerScore = 0
+    playRound(cChoice, hChoice);
+    playRound(cChoice, hChoice);
+ 
+} 
+
+
+
+const cChoice = getComputerChoice();
+const hChoice = getHumanChoice();
+playGame()
+
+
